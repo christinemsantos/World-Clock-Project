d2e7243bb568fc038a9ea03f36e8d494294c8941
Christine Santos <56892403+christinemsantos@users.noreply.github.com>
2024-01-26 13:06:29 -0500

    connected moment timezone js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+function updateTime() {
+  let newYorkDate = document.querySelector("#ny-date");
+  newYorkDate.innerHTML = moment()
+    .tz("America/New_York")
+    .format("MMMM Do YYYY");
 
+  let losAngDate = document.querySelector("#la-date");
+  losAngDate.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("MMMM Do YYYY");
+
+  let rioDate = document.querySelector("#sydney-date");
+  rioDate.innerHTML = moment().tz("Australia/Sydney").format("MMMM Do YYYY");
+
+  let berlinDate = document.querySelector("#berlin-date");
+  berlinDate.innerHTML = moment().tz("Europe/Berlin").format("MMMM Do YYYY");
+
+  let newYorkTime = document.querySelector("#ny-time");
+  newYorkTime.innerHTML = moment().tz("America/New_York").format("h:mm:ss A");
+
+  let losAngTime = document.querySelector("#la-time");
+  losAngTime.innerHTML = moment().tz("America/Los_Angeles").format("h:mm:ss A");
+
+  let rioTime = document.querySelector("#sydney-time");
+  rioTime.innerHTML = moment().tz("Australia/Sydney").format("h:mm:ss A");
+
+  let berlinTime = document.querySelector("#berlin-time");
+  berlinTime.innerHTML = moment().tz("Europe/Berlin").format("h:mm:ss A");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
